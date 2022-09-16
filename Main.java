c8783e8873aa1cff0169afd52f0d1f077e04a6e9
Jakemove <109541834+Jakemove@users.noreply.github.com>
2022-09-16 16:20:28 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,61 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World!");
+	
+	//değişken isimlendirilmeleri java'da camelCase yazılır.
+	
+	String ortaMetin = "İlginizi Çekebilir";
+	String altMetin = "Vade süresi";
+	
+	System.out.println(ortaMetin);
+	
+	//integer
+	int vade = 12;
+	
+	double dolarDun=18.15;
+	double dolarBugun= 18.15;
+	
+	boolean dolarDustuMu = true;
+	String okYonu;
+	 okYonu = "down.svg";
+	if (dolarBugun<dolarDun) {
+		System.out.println(okYonu);
+		
+	} else if(dolarBugun>dolarDun) {
+		 okYonu = "up.svg";
+		System.out.println(okYonu);
+		
+
+	}
+	else {
+		System.out.println("equel.svg");
+	}
+	//array
+	String[] krediler = {"hızlı", "emekli", "mutlu"};
+	
+	
+	for (int i = 0; i < krediler.length; i++)
+	{
+		System.out.println(krediler[i]);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
+
+
